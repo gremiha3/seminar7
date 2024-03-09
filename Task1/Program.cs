e4ac928fcbf5f023ff61fa3fea05fa2444624dec
Nikolay Kozlov <gremiha3@gmail.com>
2024-03-09 21:56:25 +0300

    Task1 is done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void ListNumbers(int M, int N)
+{
+    if (M == N)
+    {
+        Console.Write($"{N}, ");
+        return;
+    }
+    if (M < N)
+    {
+        ListNumbers(M, N - 1);
+        Console.Write($"{N}, ");
+    }
+    if (M > N)
+    {
+        ListNumbers(M - 1, N);
+        Console.Write($"{M}, ");
+    }
+}
+
+
+Console.WriteLine("Введите натуральное число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write($"M = {M}; N = {N} -> \"");
+ListNumbers(M, N);
+Console.WriteLine("\"");
